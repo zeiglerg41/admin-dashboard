98fbf607d9fdef2df667619ef940de5390bc52da
Gwary <zeiglerg41@gmail.com>
2024-02-18 13:40:54 -0700

    Repeat gql fetch data for dashboard deals card

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,9 +1,86 @@
-
+import { DollarOutlined } from "@ant-design/icons";
+import { Card } from "antd";
+import { Text } from "../text";
+import { Area, AreaConfig } from "@ant-design/plots";
+import { useList } from "@refinedev/core";
+import { DASHBOARD_DEALS_CHART_QUERY } from "@/graphql/queries";
+import { mapDealsData } from "@/utilities/helpers";
+import React from "react";
+import { GetFieldsFromList } from "@refinedev/nestjs-query";
+import { DashboardDealsChartQuery } from "@/graphql/types";
 
 const DealsChart = () => {
-    return (
-        <div>DealsChart</div>
-    )
-}
+  const { data } = useList<GetFieldsFromList<DashboardDealsChartQuery>>({
+    //hook by refine that allows you to fetch data from API
+    resource: "dealStages",
+    filters: [
+      {
+        field: "title",
+        operator: "in",
+        value: ["WON", "LOST"],
+      },
+    ],
+    meta: {
+      // need to point to graphql query that returns all events
+      gqlQuery: DASHBOARD_DEALS_CHART_QUERY,
+    },
+  }); // Define which list to use and what data to return
+  const dealData = React.useMemo(() => {
+    return mapDealsData(data?.data);
+  }, [data?.data]);
+
+  const config: AreaConfig = {
+    // get data property
+    data: dealData,
+    xField: "timeText",
+    yField: "value",
+    isStack: false,
+    seriesField: "state",
+    animation: true,
+    startOnZero: false,
+    smooth: true,
+    legend: {
+      offsetY: -6,
+    },
+    yAxis: {
+      tickCount: 4,
+      label: {
+        formatter: (v: string) => {
+          return `$${Number(v) / 1000}k`;
+        },
+      },
+    },
+    tooltip: {
+      formatter: (data) => {
+        return {
+          name: data.state,
+          value: `${Number(data.value) / 1000}k`,
+        };
+      },
+    },
+  };
+  return (
+    <Card
+      style={{ height: "100%" }}
+      title={
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "8px",
+            padding: "8px 16px", // Apply padding directly to the title content
+          }}
+        >
+          <DollarOutlined />
+          <Text size="sm" style={{ marginLeft: "0.5rem" }}>
+            Deals
+          </Text>
+        </div>
+      }
+    >
+      <Area {...config} height={325} />
+    </Card>
+  );
+};
 
 export default DealsChart;
