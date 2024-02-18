0d490428f9a541d6129978a6701be7a241b52e5c
Gwary <zeiglerg41@gmail.com>
2024-02-18 09:16:35 -0700

    Modify User Avatar position in header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,10 +1,24 @@
-import CurrentUser from "./current-user"
-const Header = () => {
-  return (
-    <div>
-        <CurrentUser />
-    </div>
-  )
-}
+import { Layout, Space } from "antd";
+import CurrentUser from "./current-user";
 
-export default Header
+const Header = () => {
+  const headerStyles: React.CSSProperties = {
+    background: "#fff",
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: "0 20px",
+    position: "sticky",
+    top: 0,
+    zIndex: 999,
+  };
+  return (
+    <Layout.Header style={headerStyles}>
+      <Space align="center" size="middle">
+        <CurrentUser />
+      </Space>
+    </Layout.Header>
+  );
+};
+
+export default Header;
