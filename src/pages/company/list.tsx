02619854d57fff7b087a6b2943060dcb1837d1ab
Gwary <zeiglerg41@gmail.com>
2024-02-19 11:17:09 -0700

    Implement filter ability for company names and sort by newest company based on deal sum value

--- src/pages/company/list.tsx
+++ src/pages/company/list.tsx
@@ -4,7 +4,14 @@ import { COMPANIES_LIST_QUERY } from "@/graphql/queries";
 import { Company } from "@/graphql/schema.types";
 import { currencyNumber } from "@/utilities";
 import { SearchOutlined } from "@ant-design/icons";
-import { CreateButton, FilterDropdown, List, useTable } from "@refinedev/antd";
+import {
+  CreateButton,
+  DeleteButton,
+  EditButton,
+  FilterDropdown,
+  List,
+  useTable,
+} from "@refinedev/antd";
 import { getDefaultFilter, useGo } from "@refinedev/core";
 import { Input, Space, Table } from "antd";
 
@@ -13,8 +20,34 @@ export const CompanyList = () => {
 
   const { tableProps, filters } = useTable({
     resource: "companies",
+    onSearch: (value: any) => {
+      return [
+        {
+          field: "name",
+          operator: "contains",
+          value: value.name,
+        },
+      ];
+    },
     pagination: {
       pageSize: 12,
+    },
+    sorters: {
+      initial: [
+        {
+          field: "createdAt",
+          order: "desc",
+        },
+      ],
+    },
+    filters: {
+      initial: [
+        {
+          field: "name",
+          operator: "contains",
+          value: undefined,
+        },
+      ],
     },
     meta: {
       gqlQuery: COMPANIES_LIST_QUERY,
@@ -65,7 +98,9 @@ export const CompanyList = () => {
                 name={record.name}
                 src={record.avatarUrl}
               />
-              <Text style={{ whiteSpace: "nowrap" }} strong>{record.name}</Text>
+              <Text style={{ whiteSpace: "nowrap" }} strong>
+                {record.name}
+              </Text>
             </Space>
           )}
         />
@@ -76,6 +111,17 @@ export const CompanyList = () => {
             <Text>
               {currencyNumber(company?.dealsAggregate?.[0].sum?.value || 0)}{" "}
             </Text>
+          )}
+        />
+        <Table.Column<Company>
+          dataIndex="id"
+          title="Actions"
+          fixed="right"
+          render={(value) => (
+            <Space>
+              <EditButton hideText size="small" recordItemId={value} />
+              <DeleteButton hideText size="small" recordItemId={value} />
+            </Space>
           )}
         />
       </Table>
