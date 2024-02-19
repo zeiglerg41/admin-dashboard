f49468e4a362c78bc03231bf59fd583ca55fbbfe
Gwary <zeiglerg41@gmail.com>
2024-02-19 15:17:09 -0700

    Modified EditPage path

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,23 @@ import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import { useNotificationProvider } from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
 
-import { dataProvider, liveProvider } from "./Providers";
+import { Home, ForgotPassword, Login, Register, CompanyList } from "./pages";
+
+import { dataProvider, liveProvider, authProvider } from "./Providers";
 import routerBindings, {
   CatchAllNavigate,
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
+
 import { App as AntdApp } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-import { authProvider } from "./Providers/auth";
-import { Home, ForgotPassword, Login, Register, CompanyList } from "./pages";
 import Layout from "./components/layout";
 import { resources } from "./config/resources";
 import Create from "./pages/company/create";
-import Edit from "./pages/company/edit";
+import EditPage from "./pages/company/edit";
 
 function App() {
   return (
@@ -65,9 +67,9 @@ function App() {
                 >
                   <Route index element={<Home />} />
                   <Route path="/companies">
-                    <Route index element={<CompanyList/>} />
-                    <Route path="new" element={<Create/>} />
-                    <Route path="edit/:id" element={<Edit/>} />
+                    <Route index element={<CompanyList />} />
+                    <Route path="new" element={<Create />} />
+                    <Route path="edit/:id" element={<EditPage />} />
                   </Route>
                 </Route>
               </Routes>
