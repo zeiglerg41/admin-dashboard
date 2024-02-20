aebf3d235cd6287eba9ae21d07034c809d64584e
Gwary <zeiglerg41@gmail.com>
2024-02-20 08:36:14 -0700

    explicit import/export fo authCredentials

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,5 +1,6 @@
 import { dataProvider } from "./data";
 import { liveProvider } from "./data";
 import { authProvider } from "./auth";
+import { authCredentials } from "./auth";
 
-export { dataProvider, liveProvider, authProvider };
+export { dataProvider, liveProvider, authProvider, authCredentials };
